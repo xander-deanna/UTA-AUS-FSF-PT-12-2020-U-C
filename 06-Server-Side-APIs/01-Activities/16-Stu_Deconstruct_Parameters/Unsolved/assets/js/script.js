777cec665e0d827394ec43deaafe2c4892a74ac9
Deanna <xander.deanna@gmail.com>
2021-01-27 18:17:39 -0600

    pushing changes to pull class repo updates

--- 06-Server-Side-APIs/01-Activities/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
@@ -9,3 +9,11 @@ fetch(
     console.log(data);
   });
 // Parameter explanation.
+
+//Per_page = 10 results per page
+//state = issues that are in an open state
+//sort = how the results are sorted (based on creation date)
+//direction = decending or ascending
+
+//A query string is a part of a uniform resource locator (URL) that assigns values to specified parameters
+//the portion of a URL where data is passed to a web application and/or back-end database.
--- 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
@@ -4,4 +4,7 @@ var redirectUrl = './404.html';
 fetch(badRequestUrl).then(function (response) {
   // Use a conditional to check the response status.
   // If that status equals the conditional, then redirect to the 404 page.
+  if (response.status === 404){
+    document.location.replace(redirectUrl);
+  }
 });
